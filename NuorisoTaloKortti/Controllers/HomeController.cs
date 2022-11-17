f82d0f23baf0d5b798397b630ec8cf1e1c6e7328
Juri Tökvin <juri.tokvin@student.careeria.fi>
2022-11-17 12:20:10 +0200

    Lisätty Kirjautumis Tarkistus

--- NuorisoTaloKortti/Controllers/HomeController.cs
+++ NuorisoTaloKortti/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using NuorisoTaloKortti.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -26,5 +27,29 @@ namespace NuorisoTaloKortti.Controllers
 
             return View();
         }
+
+
+        [HttpPost]
+        public ActionResult Authorize(Kayttajat kayttajat)
+        {
+            NuorisokorttiEntities db = new NuorisokorttiEntities();
+            //Haetaan käyttäjän/Loginin tiedot annetuilla tunnustiedoilla tietokannasta LINQ -kyselyllä
+            var LoggedUser = db.Kayttajat.SingleOrDefault(x => x.Kayttajanimi == kayttajat.Kayttajanimi && x.Salasana == kayttajat.Salasana);
+            if (LoggedUser != null)
+            {
+                ViewBag.LoginMessage = "Successfull login";
+                ViewBag.LoggedStatus = "In";
+                Session["Kayttajanimi"] = LoggedUser.Kayttajanimi;
+                return RedirectToAction("About", "Home"); //Tässä määritellään mihin onnistunut kirjautuminen johtaa --> Home/Index
+            }
+            else
+            {
+                ViewBag.LoginMessage = "Login unsuccessfull";
+                ViewBag.LoggedStatus = "Out";
+                //Kayttajat.LoginErrorMessage = "Tuntematon käyttäjätunnus tai salasana.";
+                return View("Login", kayttajat);
+            }
+        }
     }
+
 }
