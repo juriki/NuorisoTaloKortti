7547d7fc1c08aa6290dc7c68ba05829dfffd3900
AnttiVaaranen <antti.vaaranen@gmail.com>
2022-12-21 12:26:56 +0200

    Crud-toiminnot täydennetty huoltajille

--- NuorisoTaloKortti/Controllers/HuoltajatController.cs
+++ NuorisoTaloKortti/Controllers/HuoltajatController.cs
@@ -28,53 +28,131 @@ namespace NuorisoTaloKortti.Controllers
             return RedirectToAction("Loginikkuna", "Home");
         }
 
+        public ActionResult Create()
+        {
+            if (Session["Kayttajanimi"] != null && Session["Yllapito"].ToString() == "True")
+            {
+                var post = db.Postitoimipaikat;
+                IEnumerable<SelectListItem> selectPostList = from p in post
+                                                             select new SelectListItem
+                                                             {
+                                                                 Value = p.Postinumero,
+                                                                 Text = p.Postinumero + " " + p.Postitoimipaikka
+                                                             };
+
+                ViewBag.Postinumero = new SelectList(selectPostList, "Value", "Text");
+
+                return View();
+            }
+
+            return RedirectToAction("Loginikkuna", "Home");
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Etunimi, Sukunimi, Puhelinnumero, Osoite, Postinumero")] Huoltajat huoltaja)
+        {
+            if (Session["Kayttajanimi"] != null && Session["Yllapito"].ToString() == "True")
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Huoltajat.Add(huoltaja);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(huoltaja);
+
+            }
+           
+            return RedirectToAction("Loginikkuna", "Home");
+
+        }
+
         public ActionResult Edit(int? id)
         {
-            //MessageBox.Show(id.ToString());
-            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            Huoltajat huoltaja = db.Huoltajat.Find(id);
-            if (huoltaja == null) return HttpNotFound();
-            var post = db.Postitoimipaikat;
-            IEnumerable<SelectListItem> selectPostList = from p in post
-                                                         select new SelectListItem
-                                                         {
-                                                             Value = p.Postinumero,
-                                                             Text = p.Postinumero + " " + p.Postitoimipaikka
-                                                         };
-         
-            ViewBag.Postinumero = new SelectList(selectPostList, "Value", "Text", huoltaja.Postinumero);
+            if (Session["Kayttajanimi"] != null && Session["Yllapito"].ToString() == "True")
+            {
+                if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                Huoltajat huoltaja = db.Huoltajat.Find(id);
+                if (huoltaja == null) return HttpNotFound();
+                var post = db.Postitoimipaikat;
+                IEnumerable<SelectListItem> selectPostList = from p in post
+                                                             select new SelectListItem
+                                                             {
+                                                                 Value = p.Postinumero,
+                                                                 Text = p.Postinumero + " " + p.Postitoimipaikka
+                                                             };
 
+                ViewBag.Postinumero = new SelectList(selectPostList, "Value", "Text", huoltaja.Postinumero);
 
+                return View(huoltaja);
+            }
 
-            return View(huoltaja);
+            return RedirectToAction("Loginikkuna", "Home");
+
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "HuoltajaID, Etunimi, Sukunimi, Puhelinnumero, Osoite, Postinumero")] Huoltajat huoltaja)
         {
-
-            
-            if (ModelState.IsValid)
+            if (Session["Kayttajanimi"] != null && Session["Yllapito"].ToString() == "True")
             {
-                db.Entry(huoltaja).State = EntityState.Modified;
 
+                if (ModelState.IsValid)
+                {
+                    db.Entry(huoltaja).State = EntityState.Modified;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                var post = db.Postitoimipaikat;
+                IEnumerable<SelectListItem> selectPostList = from p in post
+                                                             select new SelectListItem
+                                                             {
+                                                                 Value = p.Postinumero,
+                                                                 Text = p.Postinumero + " " + p.Postitoimipaikka
+                                                             };
+
+                ViewBag.Postinumero = new SelectList(selectPostList, "Value", "Text", huoltaja.Postinumero);
+
+                return View(huoltaja);
+            }
+
+            return RedirectToAction("Loginikkuna", "Home");
+
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (Session["Kayttajanimi"] != null && Session["Yllapito"].ToString() == "True")
+            {
+                if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                Huoltajat huoltaja = db.Huoltajat.Find(id);
+                if (huoltaja == null) return HttpNotFound();
+                return View(huoltaja);
+            }
+            return RedirectToAction("Loginikkuna", "Home");
+
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            if (Session["Kayttajanimi"] != null && Session["Yllapito"].ToString() == "True")
+            {
+                Huoltajat huoltaja = db.Huoltajat.Find(id);
+                db.Huoltajat.Remove(huoltaja);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
+            return RedirectToAction("Loginikkuna", "Home");
 
-            var post = db.Postitoimipaikat;
-            IEnumerable<SelectListItem> selectPostList = from p in post
-                                                         select new SelectListItem
-                                                         {
-                                                             Value = p.Postinumero,
-                                                             Text = p.Postinumero + " " + p.Postitoimipaikka
-                                                         };
-
-            ViewBag.Postinumero = new SelectList(selectPostList, "Value", "Text", huoltaja.Postinumero);
-
-            return View(huoltaja);
-            
         }
+
+
     }
 }
