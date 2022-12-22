5acd5067bf2a4a87a2b0d448d9937e21800d143e
Juri Tökvin <juri.tokvin@student.careeria.fi>
2022-12-22 11:22:22 +0200

    Toimiva

--- NuorisoTaloKortti/Controllers/KorttiController.cs
+++ NuorisoTaloKortti/Controllers/KorttiController.cs
@@ -140,10 +140,8 @@ namespace NuorisoTaloKortti.Controllers
         [HttpPost]
         public ActionResult PsswordChange([Bind(Include = "KayttajaId,Kayttajanimi, Salasana, ErrorMessage,uusiSalasana,ToistaSalasana")] Kayttajat kayttajat)
         {
-            List<Kayttajat> model = db.Kayttajat.ToList();
-            Kayttajat kay  = db.Kayttajat.Find(kayttajat.KayttajaId);
 
-            if (kayttajat.uusiSalasana.Length < 8 )
+            if (kayttajat.uusiSalasana.Length < 8)
             {
                 kayttajat.LoginErrorMessage = "Salasana Pitä olla vähintäin 8 merkkinen";
                 return View(kayttajat);
@@ -155,7 +153,7 @@ namespace NuorisoTaloKortti.Controllers
                 MessageBox.Show(kayttajat.uusiSalasana.ToString());
                 return View(kayttajat);
             }
-            if(kayttajat.Salasana == null || kay.Salasana != kayttajat.Salasana) 
+            if (kayttajat.Salasana == null)
             {
                 kayttajat.LoginErrorMessage = "Vanha salasana puuttu tai väärin kirjoitettu";
                 return View(kayttajat);
