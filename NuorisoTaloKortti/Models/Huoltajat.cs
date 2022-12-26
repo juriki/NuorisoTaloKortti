2afa8c00694805f052f16fc62518bb91a5086db1
Juri Tökvin <juri.tokvin@student.careeria.fi>
2022-12-26 16:51:51 +0200

    Salasanan Hash toiminto lisätty Myös oletus salasanan vaihto edotus lisätty kortti näkymään

--- NuorisoTaloKortti/Models/Huoltajat.cs
+++ NuorisoTaloKortti/Models/Huoltajat.cs
@@ -11,8 +11,7 @@ namespace NuorisoTaloKortti.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class Huoltajat
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -27,7 +26,7 @@ namespace NuorisoTaloKortti.Models
         public string Puhelinnumero { get; set; }
         public string Osoite { get; set; }
         public string Postinumero { get; set; }
-        
+    
         public virtual Postitoimipaikat Postitoimipaikat { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Nuoret> Nuoret { get; set; }
