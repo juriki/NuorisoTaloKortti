2e93b8e546e4292447249bfb9176d9fb64818b08
Juri Tökvin <juri.tokvin@student.careeria.fi>
2023-03-26 17:13:26 +0300

    problems

--- NuorisoTaloKortti/Models/Kayttajat.cs
+++ NuorisoTaloKortti/Models/Kayttajat.cs
@@ -11,12 +11,26 @@ namespace NuorisoTaloKortti.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Kayttajat
     {
         public int KayttajaId { get; set; }
+        [Required(ErrorMessage = "Käyttäjätunnus on pakollinen tieto!")]
         public string Kayttajanimi { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Anna salasana!")]
         public string Salasana { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Anna salasana!")]
+        public string uusiSalasana { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Anna salasana!")]
+        public string ToistaSalasana { get; set; }
         public bool Yllapito { get; set; }
+        public string LoginErrorMessage { get; internal set; }
     }
 }
