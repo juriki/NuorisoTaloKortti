901f84e5232f384e2b2d86a001c274bcb6e16d79
Juri Tökvin <juri.tokvin@student.careeria.fi>
2023-04-10 16:03:38 +0300

    ok

--- NuorisoTaloKortti/Models/Huoltajat.cs
+++ NuorisoTaloKortti/Models/Huoltajat.cs
@@ -29,5 +29,6 @@ namespace NuorisoTaloKortti.Models
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Nuoret> Nuoret { get; set; }
+        public virtual Postitoimipaikat Postitoimipaikat { get; set; }
     }
 }
